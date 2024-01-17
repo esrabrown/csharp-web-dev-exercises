3368c8cd33f06112a0c6748e10e1cca4ce72e3c6
esrabrown <essrcskn@hotmail.com>
2024-01-17 14:38:05 -0600

    practicing c#

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,3 +4,16 @@
 
 Console.WriteLine("Hello, Strings!");
 
+string words = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse."
+
+
+//Use the Split method to divide the string at each space and store the individual words in an array. If you need to review the method syntax, look back at the string methods table.
+
+string[] wordsArray = words.Split(' ');
+
+foreach(string word in wordsArray)
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine(string.Join(",", wordsArray));
