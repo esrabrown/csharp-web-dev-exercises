049949a46e47b2fe6e0c65371afa1fab523ba923
esrabrown <essrcskn@hotmail.com>
2024-01-17 14:20:25 -0600

    practicing csharp

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,3 +4,13 @@
 
 Console.WriteLine("Hello, Arrays!");
 
+int[] arry = {1, 1, 2, 3, 5, 8};
+
+foreach (int num in arry)
+{
+   //  Console.WriteLine(num); prints each num
+   if( num % 2 !=0)
+   {
+    Console.WriteLine(num); //prints odd nums
+   }
+}
